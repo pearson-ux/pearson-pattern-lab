515834e34f8e176b3b3372a2bd0a4c7b51b05a07
Anne Hong <anne.hong@pearson.com>
2019-01-11 19:15:46 -0800

    Added external link icon

--- glp-writing-solutions-pl/js/iconList.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function() {
-  const flipContainers = document.querySelectorAll(".flip-container");
-  flipContainers.forEach(flipContainer => {
-    flipContainer.addEventListener("click", event => {
-      flipContainer.classList.toggle("flip");
-    });
-  });
-})();
